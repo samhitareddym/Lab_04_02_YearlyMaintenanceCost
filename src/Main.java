da3775c00b0fbfc994b52fbe6a76d187cb796115
samhitareddy <samhitareddy0402@gmail.com>
2024-09-23 23:25:39 -0400

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        // Prompt the user for maintenance costs for each season
+        System.out.print("Enter the maintenance cost for Spring: $");
+        double springCost = scanner.nextDouble();
+
+        System.out.print("Enter the maintenance cost for Summer: $");
+        double summerCost = scanner.nextDouble();
+
+        System.out.print("Enter the maintenance cost for Fall: $");
+        double fallCost = scanner.nextDouble();
+
+        System.out.print("Enter the maintenance cost for Winter: $");
+        double winterCost = scanner.nextDouble();
+
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+
+        System.out.println ("Maintenance Costs");
+        System.out.println ("Spring: " + springCost);
+        System.out.println ("Summer: " + summerCost);
+        System.out.println ("Fall: " + fallCost);
+        System.out.println ("Winter: " +  winterCost);
+        System.out.println ("Total Yearly Maintenance Cost: " + totalCost);
+
+        scanner.close();
     }
 }
